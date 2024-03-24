16c1af469eb972da2e618892ca29c44e197132ed
Maya-Gersten10 <162187253+Maya-Gersten10@users.noreply.github.com>
2024-03-24 13:45:05 -0400

    Add files via upload

---
+++ Proposal/Proposal.md
@@ -0,0 +1,64 @@
+# Project Proposal Example
+
+## Project Choice (Tell us which project you're doing!)
+
+- Connect 4
+
+## Project Description 
+
+Magical Tic Tac Toe uses the mascots of the Hogwarts houses:
+Gryffindor: Lion
+Slytherin: Serpent
+Hufflepuff: Badger
+Ravenclaw: Black bird
+Instead of X's and O's. So, players would take turns placing their respective house mascots on the grid, aiming to get four of their house mascots in a row horizontally, vertically, or diagonally to win the game. 
+
+## Wire Frames
+
+**Initial Landing View**
+
+![image](file:///Users/mayagersten/Desktop/PROPOSAL%20IMG.png)
+
+**Results View**
+
+![image](file:///Users/mayagersten/Desktop/PROPOSAL%20IMG.png)
+
+## User Stories
+
+#### MVP Goals
+
+- As a player, I want my game to recognize four ticks in a row so that I know who won.
+- As a player, I would like to be informed when the game is over due to a tie, win or loss.
+- As a player, I would like to know whose turn it is so that I don't have to keep track.
+- As a player, I would like to be informed with some indication when I mark a game box so that I know my selection worked.
+- As a player I would like to be able to restart the game after a win, loss or tie.
+- As a player, I want the UI to be engaging and out of the way so that I enjoy the experience of playing the game.\*\*
+
+#### Stretch Goals
+
+- As a player, I would like a victory animation when I win the game, so that I feel good about my victory!
+- As a player, I would like to hear an audible sound when I mark a game box so that I know my selection worked.
+- As a player, I would like to play this game online so that I don't have to have my challenger sitting next to me.
+- As a player who wants to dominate the battlefield I want to become the ultimate tic tac toe tactician so I can beat all of my opponents
+- As a player who requires assistive technologies, I would like accessibility features so that I'm not left out of enjoying the game with timer.
+- Choosing a borad size.
+
+#### Notionboard Template
+Notionboard template for building projects ( You can use this for any project )
+https://www.notion.so/GA-Unit-3-Tunr-Lab-da2c82fafd4e4a7aa654676732db9ee3
+
+#### Timeline - Daily Accountability
+Example of a Timeline to keep organized and on task for hitting goals every single day you’re on the sprint for your project.
+
+Create your own table using this markdown table generator website:
+https://www.tablesgenerator.com/markdown_tables
+
+Do not neglect to plan, you will thank yourself later for being proactive!
+| Day        |   | Task                               | Blockers | Notes/ Thoughts |
+|------------|---|------------------------------------|----------|-----------------|
+| Monday   |   | Create and present proposal, Create html, js, css files        |          |                 |
+| Tuesday     |   | Create basic scaffolding, Add functionality           |          |                 |
+| Wednesday   |   | Add functionality,  Add styling,           |          |                 |
+| Thursday     |   | Finaliza MVP                       |          |                 |
+| Friday     |   |  Work on stretch goals, Presentation Day!                          |          |                 |
+
---
+++ Proposal/script.js
@@ -0,0 +1,141 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const playerButtons = document.querySelectorAll(".player");
+    const gameContainer = document.getElementById("game");
+    const messageDisplay = document.getElementById("message");
+    const resetButton = document.getElementById("reset");
+    let currentPlayerEmoji = "";
+    let currentPlayerHouse = "";
+    let computerEmoji = "";
+    let computerHouse = "";
+    let cells = [];
+    let playerTurn = true;
+
+    const emojiToHouse = {
+        "🐍": "Slytherin",
+        "🦁": "Gryffindor",
+        "🦡": "Hufflepuff",
+        "🐦‍⬛": "Ravenclaw"
+    };
+
+    function initializeGame() {
+        cells = document.querySelectorAll(".cell");
+        currentPlayerEmoji = "";
+        currentPlayerHouse = "";
+        computerEmoji = "";
+        cells.forEach(cell => {
+            cell.innerText = "";
+            cell.addEventListener("click", handleCellClick);
+        });
+        gameContainer.style.display = "none";
+        playerButtons.forEach(button => button.style.display = "block");
+        resetButton.style.display = "none";
+        playerTurn = true;
+    }
+
+    function checkWin() {
+        const winConditions = [
+            [0, 1, 2, 3],
+            [4, 5, 6, 7],
+            [8, 9, 10, 11],
+            [12, 13, 14, 15],
+            [0, 4, 8, 12],
+            [1, 5, 9, 13],
+            [2, 6, 10, 14],
+            [3, 7, 11, 15],
+            [0, 5, 10, 15],
+            [3, 6, 9, 12]
+        ];
+
+        for (let condition of winConditions) {
+            const [a, b, c, d] = condition;
+            if (cells[a].innerText !== "" && cells[a].innerText === cells[b].innerText && cells[a].innerText === cells[c].innerText && cells[a].innerText === cells[d].innerText) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkTie() {
+        return Array.from(cells).every(cell => cell.innerText !== "");
+    }
+
+    function resetGame() {
+        initializeGame();
+        messageDisplay.innerText = `Select your house to start`;
+    }
+
+    function disableCells() {
+        cells.forEach(cell => cell.removeEventListener("click", handleCellClick));
+    }
+
+    function selectPlayer(event) {
+        currentPlayerEmoji = event.target.innerText;
+        currentPlayerHouse = emojiToHouse[currentPlayerEmoji];
+        gameContainer.style.display = "block";
+        updateTurnMessage();
+        playerButtons.forEach(button => button.style.display = "none");
+        resetButton.style.display = "block";
+        const emojis = Array.from(playerButtons).map(button => button.innerText);
+        emojis.splice(emojis.indexOf(currentPlayerEmoji), 1);
+        computerEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+        computerHouse = emojiToHouse[computerEmoji]; 
+    }
+
+    function updateTurnMessage() {
+        if (playerTurn) {
+            messageDisplay.innerText = `${currentPlayerHouse}'s turn`;
+        } else {
+            messageDisplay.innerText = `${computerHouse}'s turn`;
+            if (!playerTurn) {
+                setTimeout(computerMove, 500);
+            }
+        }
+    }
+
+    function handleCellClick(event) {
+        if (!playerTurn) return;
+        const clickedCell = event.target;
+        if (clickedCell.innerText === "" && currentPlayerEmoji !== "") {
+            clickedCell.innerText = currentPlayerEmoji;
+            if (checkWin()) {
+                messageDisplay.innerText = `${currentPlayerHouse} wins!`;
+                console.log("Player wins!");
+                document.getElementById("winSound").play(); 
+                disableCells();
+            } else if (checkTie()) {
+                messageDisplay.innerText = "It's a tie!";
+            } else {
+                playerTurn = false;
+                updateTurnMessage();
+            }
+        }
+    }
+
+    function computerMove() {
+        let emptyCells = Array.from(cells).filter(cell => cell.innerText === "");
+        if (emptyCells.length > 0) {
+            const randomIndex = Math.floor(Math.random() * emptyCells.length);
+            const selectedCell = emptyCells[randomIndex];
+            selectedCell.innerText = computerEmoji;
+            if (checkWin()) {
+                messageDisplay.innerText = "Computer wins!";
+                console.log("Computer wins!");
+                document.getElementById("loseSound").play(); 
+                disableCells();
+            } else if (checkTie()) {
+                messageDisplay.innerText = "It's a tie!";
+            } else {
+                playerTurn = true;
+                updateTurnMessage();
+            }
+        }
+    }
+
+    messageDisplay.innerText = "Select your house to start";
+
+    playerButtons.forEach(button => button.addEventListener("click", selectPlayer));
+
+    resetButton.addEventListener("click", resetGame);
+
+    initializeGame();
+});
